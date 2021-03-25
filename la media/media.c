c487163559354b543573437610be2f3a833d6dc4
Juan C. Mate Peña <52606424+Juan859@users.noreply.github.com>
2021-03-25 11:10:25 -0400

    C/C++
    la media & la moda

---
+++ la media/media.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+float main()
+{
+    float a;
+    float b;
+    float c;
+    printf("introduzca su  primer valor");
+    scanf("%f",&a);
+    printf("introduzca su segundo valor");
+    scanf("%f",&b);
+    printf("introduzca su tercer valor");
+    scanf("%f",&c);
+     media==("a+b+c")*3;
+     printf("su resultado sera:%f",media);
+     return 0;
+
+}
